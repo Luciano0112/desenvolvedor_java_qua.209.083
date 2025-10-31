8af6d7bc066493e73182b7affff1bd05fdcc6c79
Luciao0112 <lucianolinspereira@gmail.com>
2025-10-31 19:55:27 -0300

    Aula do Dia 30/10/2025-1ªparte.

---
+++ 01_logica-de_programacao/11_lista/src/com/lista/app/App.java
@@ -0,0 +1,16 @@
+package com.lista.app;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        //instancia a classe Scanner
+        Scanner leia = new Scanner(System.in);
+
+        //fecha objeto leia
+        leia.close();
+
+        
+    }
+}
