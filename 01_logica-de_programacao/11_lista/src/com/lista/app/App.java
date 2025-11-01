91b59b42f42cdb0333d672333224fdbed04502e6
Luciao0112 <lucianolinspereira@gmail.com>
2025-10-31 21:42:09 -0300

    Aula do Dia 31/10/2025.

--- 01_logica-de_programacao/11_lista/src/com/lista/app/App.java
+++ 01_logica-de_programacao/11_lista/src/com/lista/app/App.java
@@ -5,8 +5,20 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        //instancia a classe Scanner
-        Scanner leia = new Scanner(System.in);
+        //instancias
+        Scanner leia = new Scanner(System.in);  //Scanner
+        String [] nomes = new String[10]; //array
+
+        //entrada de dados
+        for (int i = 0; i < nomes.length;i++) {
+            System.out.println("Informe o " + (i+1) + " º nome:");
+            nomes[i] = leia.nextLine();
+        }
+
+        //saída de dados 
+        for( String nome : nomes) {
+            System.out.println(nome);
+        }
 
         //fecha objeto leia
         leia.close();
