bf3687ad3adc5d49f58e795fa58ab65eb02dadc0
Luciano0112 <lucianolinspereira@gmail.com>
2025-11-10 21:40:42 -0300

    Aula do Dia 10/11/2025.

---
+++ 02_orientacao_a_objetos/05_encapsulamento/src/com/encapsulamento/app/App.java
@@ -0,0 +1,35 @@
+package com.encapsulamento.app;
+
+import java.util.Scanner;
+
+import com.encapsulamento.models.Pessoa;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       //instancia as classes
+       Scanner leia = new Scanner(System.in);
+       Pessoa usuario = new Pessoa();
+
+       //entrada de dadod
+       System.out.println("Informe o seu nome:");
+       usuario.setNome(leia.nextLine());
+       System.out.println("Informe o seu CPF.:");
+       usuario.setCpf(leia.nextLine());
+       System.out.println("Informe o seu e-mail:");
+       usuario.setEmail(leia.nextLine());
+       System.out.println("Informe sua idade:");
+       usuario.setIdade(leia.nextInt());
+
+       //saída de dados
+       System.out.println("/n DADOS DO USUÁRIO:/n");
+       System.out.println("Nome: " + usuario.getNome());
+       System.out.println("CPF: " + usuario.getCpf());
+       System.out.println("E-mail: " + usuario.getEmail());
+       System.out.println("Idade: " + usuario.getIdade());
+
+
+       //fecha o objeto leia
+       leia.close();
+
+    }
+}
